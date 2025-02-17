2db155ab153a642682d6f1d8151e681a5f255d14
Simon-Seke <simon87persson@gmail.com>
2025-02-17 22:52:26 +0100

    Click and move inventory #1 done

--- gui/inventory_gui.gd
+++ gui/inventory_gui.gd
@@ -6,16 +6,34 @@ signal closed
 var isOpen: bool = false
 
 @onready var inventory: Inventory = preload("res://inventory/playerInventory.tres")
+@onready var itemStackGuiClass = preload("res://gui/itemsStackGui.tscn")
 @onready var slots: Array = $NinePatchRect/GridContainer.get_children()
 
 func _ready():
+	connectSlots()
 	inventory.updated.connect(update)
 	update()
+	
+func connectSlots():
+	for slot in slots:
+		var callable = Callable(onSlotClicked)
+		callable = callable.bind(slot)
+		slot.pressed.connect(callable)
 
 func update():
 	for i in range(min(inventory.slots.size(), slots.size())):
-		slots[i].update(inventory.slots[i])
+		var inventorySlot: InventorySlot = inventory.slots[i]
+		
+		if !inventorySlot.item: continue
+		
+		var itemStackGui: ItemStackGui = slots[i].itemStackGui
+		if !itemStackGui:
+			itemStackGui = itemStackGuiClass.instantiate()
+			slots[i].insert(itemStackGui)
 	
+		itemStackGui.inventorySlot = inventorySlot
+		itemStackGui.update()
+
 func open():
 	visible = true
 	isOpen = true
@@ -25,3 +43,6 @@ func close():
 	visible = false
 	isOpen = false
 	closed.emit()
+
+func onSlotClicked(slot):
+	pass
